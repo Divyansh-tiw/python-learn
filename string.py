39af9a9c8e0a3cd049ae8439eebeace6126fe189
Divyansh-tiw <rt26092000@gmail.com>
2025-09-02 20:09:08 +0530

    strings

---
+++ string.py
@@ -0,0 +1,31 @@
+# s1 = "Divyansh"
+# s2 = "Tiwari"
+# l1 = len(s1)
+# l2 = len(s2)
+
+# print(s1)
+# print(s2)
+# print(l1)
+# print(l2)
+
+# s = s1+" "+s2
+# l = len(s)
+
+# print(s)
+# print(l)
+
+# s = "hello world"
+# print(s[0])
+
+# s = "world is huge"
+# print(s[0:5])
+# print(s[5:])
+# print(s[5: len(s)])
+
+# s = "lets code in python"
+
+# print(s.endswith("on"))
+# print(s.capitalize())
+# print(s.find("code"))
+# print(s.count('o'))
+# print(s.replace('o', 'i'))
